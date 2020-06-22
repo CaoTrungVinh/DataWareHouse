354d9a64a4c66b1f0651ea7be68bc4a6e48823c3
tannghia025 <55869601+tannghia025@users.noreply.github.com>
2020-06-23 06:31:03 +0700

    update thêm hệ thống mail :memo:

--- DataWareHouse/src/model/Download.java
+++ DataWareHouse/src/model/Download.java
@@ -8,20 +8,29 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
 import java.util.LinkedList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import mail.SendMailSSL;
+
 public class Download {
 	private String sid = null;
 	private String url;
 	private String folderPath;
+	private StringBuffer noticeLogin;
+	private StringBuffer noticeDownLoad;
+	private SendMailSSL mail;
 
 	public Download(String url) {
 		this.url = url;
 		this.folderPath ="/ECEP/song.nguyen/DW_2020/data";
+		this.noticeLogin= new StringBuffer("[THÔNG BÁO] HỆ THỐNG TRUYỀN DỮ LIỆU");
+		this.noticeDownLoad = new StringBuffer("[THÔNG BÁO] HỆ THỐNG DOWNLOAD FILE");
+		this.mail = new SendMailSSL();
 	}
 	
 
@@ -33,10 +42,39 @@ public class Download {
 	public void setFolderPath(String folderPath) {
 		this.folderPath = folderPath;
 	}
+	
+	
+
+	public StringBuffer getNotice() {
+		return noticeLogin;
+	}
+
+
+	public void setNotice(StringBuffer notice) {
+		this.noticeLogin = notice;
+	}
+	
+
+
+	public StringBuffer getNoticeDownLoad() {
+		return noticeDownLoad;
+	}
+
+
+	public void setNoticeDownLoad(StringBuffer noticeDownLoad) {
+		this.noticeDownLoad = noticeDownLoad;
+	}
+	
+
+
+	public SendMailSSL getMail() {
+		return mail;
+	}
 
 
 	private void login(String username, String password) throws Exception {
 		// url
+		noticeLogin.append("\nBạn vùa mới đăng nhật vào hệ thống web :" + new Date()+" \n");
 		System.out.println("-------------------Infor Connection -------------------");
 		URL urlForGetRequest = new URL(url + "/webapi/auth.cgi?api=SYNO.API.Auth&version=3&method=login&account="
 				+ username + "&passwd=" + password + "&session=FileStation&format=cookie");
@@ -67,8 +105,11 @@ public class Download {
 			System.out.println("status success: " + jsonObject.get("success"));
 			
 			System.out.println("Login Successfull ! \n------------------------------------------------------");
+			noticeLogin.append("\nSID: " + sid);
+			noticeLogin.append("\nLogin Successfull ! \n------------------------------------------------------");
 		} else {
 			System.out.println("Login Faild please check again ! \n---------------------------------------------------");
+			noticeLogin.append("Login Faild please check again ! \n---------------------------------------------------");
 		}
 	}
 
@@ -100,7 +141,8 @@ public class Download {
 					result.push((String) ((JSONObject) files.get(i)).get("name"));
 				}
 			} else {
-				System.out.println("Truy cập sai");
+				System.out.println("Không thể truy cập được vào hệ thống");
+				
 			}
 //			System.out.println(result);
 		}
@@ -140,11 +182,14 @@ public class Download {
 	}
 	public void downloadAllFile(LinkedList<String> listFile,String folderPathDownload,String des) throws Exception {
 		if(listFile.isEmpty()) {
+			noticeDownLoad.append("\nThere is no file to download, please check again ");
 			System.out.println("There is no file to download, please check again ");
 		}else {
+			noticeDownLoad.append("\nĐã DownLoad được các file : \n");
 			for (int i = 0; i < listFile.size(); i++) {
 				//Phải kiểm tra xem file đã tồn tại chưa nếu rồi thì không tải xuống nữa -- Nhớ làm sau !
 				download(folderPathDownload+"/"+listFile.get(i),des);
+				noticeDownLoad.append(listFile.get(i) +" \n");
 				
 			}
 			
@@ -155,8 +200,12 @@ public class Download {
 		String desSaveFiles = "D:\\GitHub\\DataWareHouse\\DataWareHouse\\ListFileDownload\\";
 		Download dw = new Download("http://drive.ecepvn.org:5000");
 		dw.login("guest_access", "123456");
+		dw.getMail().sendMail("[THÔNG BÁO] ĐĂNG NHẬP VÀO WEB LẤY FILE", dw.getNotice().toString());
+		
+		
 		
 		dw.downloadAllFile(dw.listFiles(dw.getFolderPath()),dw.getFolderPath(),desSaveFiles);
+		dw.getMail().sendMail("[THÔNG BÁO] HỆ THỐNG DOWNLOAD FILE", dw.getNoticeDownLoad().toString());
 
 //		dw.listFiles("/ECEP/song.nguyen/DW_2020/data");
 //		dw.download("/ECEP/song.nguyen/DW_2020/data/SinhVien.txt", "D:\\GitHub\\DataWareHouse\\DataWareHouse\\ListFileDownload\\");
