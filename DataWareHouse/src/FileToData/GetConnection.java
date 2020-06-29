2e32b577f16de7fe56c69c3331c2a609e9ce5f3c
tannghia025 <55869601+tannghia025@users.noreply.github.com>
2020-06-29 18:03:31 +0700

    Tạo một class GetConnection sử dụng properties

---
+++ DataWareHouse/src/FileToData/GetConnection.java
@@ -0,0 +1,90 @@
+package FileToData;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class GetConnection {
+	String driver = null;
+	String url = null;
+	String user = null;
+	String pass = null;
+	String databasebName = null;
+
+	public Connection getConnection(String location) {
+		String link = "config.properties";
+		Connection result = null;
+		
+		if (location.equalsIgnoreCase("control")) {
+			try (InputStream input = new FileInputStream(link)) {
+				Properties prop = new Properties();
+				prop.load(input);
+				driver = prop.getProperty("driver_local");
+				url = prop.getProperty("url_local");
+				databasebName = prop.getProperty("dbName_control");
+				user = prop.getProperty("user_local");
+				pass = prop.getProperty("pass_local");
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			
+		}
+		} else if (location.equalsIgnoreCase("staging")) {
+			try (InputStream input = new FileInputStream(link)) {
+				Properties prop = new Properties();
+				prop.load(input);
+				driver = prop.getProperty("driver_local");
+				url = prop.getProperty("url_local");
+				databasebName = prop.getProperty("dbName_staging");
+				user = prop.getProperty("user_local");
+				pass = prop.getProperty("pass_local");
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		} else if (location.equalsIgnoreCase("datawarehouse")) {
+			try (InputStream input = new FileInputStream(link)) {
+				Properties prop = new Properties();
+				prop.load(input);
+				driver = prop.getProperty("driver_local");
+				url = prop.getProperty("url_local");
+				databasebName = prop.getProperty("dName_datawarehouse");
+				user = prop.getProperty("user_local");
+				pass = prop.getProperty("pass_local");
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		} else {
+			System.out.println("Nhap ten database sai !!!");
+
+		}
+		try {
+			Class.forName(driver);
+			String connectionURL = url + databasebName;
+			try {
+				result = DriverManager.getConnection(connectionURL, user, pass);
+			} catch (SQLException e) {
+				System.out.println("Loi ket noi, kiem tra lai");
+				System.exit(0);
+				e.printStackTrace();
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Khong thay file config");
+			System.exit(0);
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+// HƯỚNG DẪN SỬ DỤNG TRƯỚC KHI DÙNG
+	public static void main(String[] args) {
+		
+		Connection conn = new GetConnection().getConnection("hihi");
+		if (conn != null) {
+			System.out.println("Thanh cong");
+		}
+	}
+}
