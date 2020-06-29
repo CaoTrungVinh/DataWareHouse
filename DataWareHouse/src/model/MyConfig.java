0040b05437b7db34ec5aad190d41dd93738b16f8
tannghia025 <55869601+tannghia025@users.noreply.github.com>
2020-06-29 17:33:08 +0700

    Cập nhật lại model MyConfig do có thay đổi

--- DataWareHouse/src/model/MyConfig.java
+++ DataWareHouse/src/model/MyConfig.java
@@ -1,173 +1,268 @@
 package model;
 
 public class MyConfig {
-	private String URLconnection;
-	private String sourcePath;
-	private String filename;
-	private String user;
+	private int id;
+	private String source_host;
+	private String user_name;
 	private String password;
-	private String dbname;
-	private String tablename;
-	private String listField;
-	private String dbnameCopy;
-	private String tablenameCopy;
-	private String listFieldCopy;
-
+	private String list_file;
+	private String folder_download;
+	private String error_dir;
+	private String success_dir;
+	private String extension_file;
+	private String file_name;
+	private String delimiter;
+	private String staging_table;
+	private String variabless;
+	private String number_cols;
+	private String query_insert_datawarehouse;
+	
+	
+	
+	
 	public MyConfig() {
-		formatField();
 	}
 
-	public MyConfig(String uRLconnection, String sourcePath, String filename, String user, String password,
-			String dbname, String tablename, String listField, String dbnameCopy, String tablenameCopy,
-			String listFieldCopy) {
-		MyConfig config = new MyConfig();
 
-		URLconnection = uRLconnection;
-		this.sourcePath = sourcePath;
-		this.filename = filename;
-		this.user = user;
+
+
+	//contructor 
+	public MyConfig(int id, String source_host, String user_name, String password, String list_file,
+			String folder_download, String error_dir, String success_dir, String extension_file, String file_name,
+			String delimiter, String staging_table, String variabless, String number_cols,
+			String query_insert_datawarehouse) {
+		super();
+		this.id = id;
+		this.source_host = source_host;
+		this.user_name = user_name;
 		this.password = password;
-		this.dbname = dbname;
-		this.tablename = tablename;
-		this.listField = listField;
-		this.dbnameCopy = dbnameCopy;
-		this.tablenameCopy = tablenameCopy;
-		this.listFieldCopy = listFieldCopy;
+		this.list_file = list_file;
+		this.folder_download = folder_download;
+		this.error_dir = error_dir;
+		this.success_dir = success_dir;
+		this.extension_file = extension_file;
+		this.file_name = file_name;
+		this.delimiter = delimiter;
+		this.staging_table = staging_table;
+		this.variabless = variabless;
+		this.number_cols = number_cols;
+		this.query_insert_datawarehouse = query_insert_datawarehouse;
 	}
 
-	public void formatField() {
-		if (this.URLconnection == "" || this.URLconnection == null) {
-			this.URLconnection = "no data";
-		}
-		if (this.sourcePath == "" || this.sourcePath == null) {
-			this.sourcePath = "no data";
-		}
-		if (this.filename == "" || this.filename == null) {
-			this.filename = "no name";
-		}
-		if (this.user == "" || this.user == null) {
-			this.user = "root";
-		}
-		if (this.password == "" || this.password == null) {
-			this.password = "123456";
-		}
-		if (this.dbname == "" || this.dbname == null) {
-			this.dbname = "staging";
-		}
-		if (this.tablename == "" || this.tablename == null) {
-			this.tablename = "";
-		}
 
-		if (this.listField == "" || this.listField == null) {
-			this.listField = "id int auto_increment";
-		}
-		if (this.dbnameCopy == "" || this.dbnameCopy == null) {
-			this.dbnameCopy = "datawarehouse";
-		}
-		if (this.tablenameCopy == "" || this.tablenameCopy == null) {
-			this.tablenameCopy = this.tablename;
-		}
-		if (this.listFieldCopy == "" || this.listFieldCopy == null) {
-			this.listFieldCopy = this.listField;
-		}
 
+
+	public int getId() {
+		return id;
 	}
 
-	public String getURLconnection() {
-		return URLconnection;
+
+
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
-	public void setURLconnection(String uRLconnection) {
-		URLconnection = uRLconnection;
+
+
+
+	public String getSource_host() {
+		return source_host;
 	}
 
-	public String getSourcePath() {
-		return sourcePath;
+
+
+
+	public void setSource_host(String source_host) {
+		this.source_host = source_host;
 	}
 
-	public void setSourcePath(String sourcePath) {
-		this.sourcePath = sourcePath;
+
+
+
+	public String getUser_name() {
+		return user_name;
 	}
 
-	public String getFilename() {
-		return filename;
+
+
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
 	}
 
-	public void setFilename(String filename) {
-		this.filename = filename;
-	}
 
-	public String getUser() {
-		return user;
-	}
 
-	public void setUser(String user) {
-		this.user = user;
-	}
 
 	public String getPassword() {
 		return password;
 	}
 
+
+
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public String getDbname() {
-		return dbname;
+
+
+
+	public String getList_file() {
+		return list_file;
 	}
 
-	public void setDbname(String dbname) {
-		this.dbname = dbname;
+
+
+
+	public void setList_file(String list_file) {
+		this.list_file = list_file;
 	}
 
-	public String getTablename() {
 
-		return tablename;
+
+
+	public String getFolder_download() {
+		return folder_download;
 	}
 
-	public void setTablename(String tablename) {
-		this.tablename = tablename;
-		if (this.tablenameCopy == "" || this.tablenameCopy == null) {
-			this.tablenameCopy = tablename;
-		}
-		
+
+
+
+	public void setFolder_download(String folder_download) {
+		this.folder_download = folder_download;
 	}
 
-	public String getListField() {
-		return listField;
+
+
+
+	public String getError_dir() {
+		return error_dir;
 	}
 
-	public void setListField(String listField) {
-		this.listField = listField;
-		if(this.listFieldCopy ==""||this.listFieldCopy==null) {
-			this.listFieldCopy = this.listField;
-		}
+
+
+
+	public void setError_dir(String error_dir) {
+		this.error_dir = error_dir;
 	}
 
-	public String getDbnameCopy() {
-		return dbnameCopy;
+
+
+
+	public String getSuccess_dir() {
+		return success_dir;
 	}
 
-	public void setDbnameCopy(String dbnameCopy) {
-		this.dbnameCopy = dbnameCopy;
+
+
+
+	public void setSuccess_dir(String success_dir) {
+		this.success_dir = success_dir;
 	}
 
-	public String getTablenameCopy() {
-		return tablenameCopy;
+
+
+
+	public String getExtension_file() {
+		return extension_file;
 	}
 
-	public void setTablenameCopy(String tablenameCopy) {
-		this.tablenameCopy = tablenameCopy;
+
+
+
+	public void setExtension_file(String extension_file) {
+		this.extension_file = extension_file;
 	}
 
-	public String getListFieldCopy() {
-		return listFieldCopy;
+
+
+
+	public String getFile_name() {
+		return file_name;
 	}
 
-	public void setListFieldCopy(String listFieldCopy) {
-		this.listFieldCopy = listFieldCopy;
-		
+
+
+
+	public void setFile_name(String file_name) {
+		this.file_name = file_name;
 	}
+
+
+
+
+	public String getDelimiter() {
+		return delimiter;
+	}
+
+
+
+
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+
+
+
+
+	public String getStaging_table() {
+		return staging_table;
+	}
+
+
+
+
+	public void setStaging_table(String staging_table) {
+		this.staging_table = staging_table;
+	}
+
+
+
+
+	public String getVariabless() {
+		return variabless;
+	}
+
+
+
+
+	public void setVariabless(String variabless) {
+		this.variabless = variabless;
+	}
+
+
+
+
+	public String getNumber_cols() {
+		return number_cols;
+	}
+
+
+
+
+	public void setNumber_cols(String number_cols) {
+		this.number_cols = number_cols;
+	}
+
+
+
+
+	public String getQuery_insert_datawarehouse() {
+		return query_insert_datawarehouse;
+	}
+
+
+
+
+	public void setQuery_insert_datawarehouse(String query_insert_datawarehouse) {
+		this.query_insert_datawarehouse = query_insert_datawarehouse;
+	}
+	
+	
+	
+	
+	
+	
 
 }
