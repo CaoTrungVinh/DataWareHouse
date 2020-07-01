ed0a5377fcaa7a4778580bf0b592414ba54acea5
tannghia025 <55869601+tannghia025@users.noreply.github.com>
2020-07-01 13:34:12 +0700

    Thêm chức năng insert vào datawarehouse khi dữ liệu chưa tồn tại

--- DataWareHouse/src/nghia/stagingtowarehouse/DataWarehouse.java
+++ DataWareHouse/src/nghia/stagingtowarehouse/DataWarehouse.java
@@ -1,9 +1,266 @@
 package nghia.stagingtowarehouse;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import FileToData.GetConnection;
+import mail.SendMailSSL;
+import model.MyConfig;
+
 public class DataWarehouse {
-	public void run() {
-		//hàm run
+	SendMailSSL sendMail = null;
+
+	public DataWarehouse() {
+		sendMail = new SendMailSSL();
 	}
-	
+
+	// HÀM RUN
+	@SuppressWarnings("null")
+	public void run() {
+		// 1. MỞ KẾT NỐI DATABASE Control_Data
+		Connection connect_control = GetConnection.getConnection("control");
+
+		// NẾU KẾT NỐI KHÔNG THÀNH CÔNG --> 2. ĐÓNG KẾT NỐI
+		if (connect_control == null) {
+			try {
+				connect_control.close();
+
+				// 3. GỬI MAIL THÔNG BÁO LỖI
+				sendMail.sendMail("[ERROR] TRANSFORM DATAWAREHOUSE", "Không thể kết nối tới database Control_data");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} else {
+			// NẾU KẾT NỐI THÀNH CÔNG --> 2.LẤY DỮ LIỆU CÓ STATUS ='TR'--> 3.TRẢ VỀ MỘT RESULTSET --> 4.1 LƯU LIỆU TRONG LIST VÀ ĐÓNG KẾT NỐI
+			List<MyConfig> listConfig = getValuesFromConfig(connect_control);
+
+			// 5. MỞ KẾT NỐI DATABASE Staging
+			Connection connect_staging = GetConnection.getConnection("staging");
+			Connection connect_warehouse = GetConnection.getConnection("datawarehouse");
+
+			// NẾU KẾT NỐI KHÔNG THÀNH CÔNG --> 6. ĐÓNG KẾT NỐI
+			if (connect_staging == null || connect_warehouse == null) {
+				try {
+					connect_staging.close();
+					connect_warehouse.close();
+					
+					// 7. GỬI MAIL THÔNG BÁO LỖI
+					sendMail.sendMail("[ERROR] TRANSFORM DATAWAREHOUSE",
+							"Không thể kết nối tới database Staging hoặc datawarehouse");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			} else {
+				// 7
+				for (MyConfig myConfig : listConfig) {
+					// CHƯA TỒN TẠI DỮ LIỆU
+
+					if (checkTableExist(connect_warehouse, myConfig.getStaging_table(), "datawarehouse") == 0) {
+						//TẠO BẢNG
+						if(createTable(myConfig, connect_warehouse)) {
+							// INSERT DỮ LIỆU TỪ STAGING QUA DATAWAREHOUSE
+							insertDatawarehouse(connect_staging, connect_warehouse, myConfig);
+						}
+						
+					}
+				}
+
+				// *7
+
+			}
+		}
+
+	}
+
+	private ArrayList<MyConfig> getValuesFromConfig(Connection connection) {
+		ArrayList<MyConfig> listConfig = new ArrayList<MyConfig>();
+		String sql = "SELECT log.id_config, conf.staging_table, conf.variabless,conf.number_cols\r\n"
+				+ "FROM config conf join logs log on conf.id = log.id_config\r\n" + "WHERE log.status = 'TR';";
+		MyConfig myConfig = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			statement = connection.prepareStatement(sql);
+			// 3. TRẢ VỀ MỘT resultSet
+			resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				myConfig = new MyConfig();
+				myConfig.setId(resultSet.getInt("id_config"));
+				myConfig.setStaging_table(resultSet.getString("staging_table"));
+				myConfig.setVariabless(resultSet.getString("variabless"));
+				myConfig.setNumber_cols(resultSet.getString("number_cols"));
+
+				// 4.1 LƯU DỮ LIỆU VÀO MỘT CÁI LIST
+				listConfig.add(myConfig);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 4.2 ĐÓNG KẾT NỐI DATABASE control_data
+			try {
+
+				if (statement != null) {
+					statement.close();
+				}
+				if (resultSet != null) {
+					resultSet.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+
+			} catch (SQLException e) {
+				System.out.println("Khong the tao bang");
+				e.printStackTrace();
+			}
+		}
+
+		return listConfig;
+	}
+
+	private void insertDatawarehouse(Connection connec_Stag, Connection connect_warehouse, MyConfig config) {
+		int cols = Integer.parseInt(config.getNumber_cols());
+
+		String sql = "SELECT * FROM " + config.getStaging_table();
+		PreparedStatement statement = null;
+		ResultSet result = null;
+		PreparedStatement statementWareHouse = null;
+		ResultSet resultWareHouse = null;
+
+		try {
+			statement = connec_Stag.prepareStatement(sql);
+			result = statement.executeQuery();
+			while (result.next()) {
+				String sqlInsert = "INSERT INTO " + config.getStaging_table() + " VALUES(";
+				for (int i = 1; i <= cols; i++) {
+					if (i == cols) {
+						sqlInsert += "'" + result.getString(i) + "');";
+					} else {
+						sqlInsert += "'" + result.getString(i) + "',";
+					}
+
+				}
+				System.out.println(sqlInsert);
+				statementWareHouse = connect_warehouse.prepareStatement(sqlInsert);
+				statementWareHouse.executeUpdate();
+				System.out.println("Successfully insert the row(s)");
+				
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 4.2 ĐÓNG KẾT NỐI DATABASE control_data
+			try {
+
+				if (statement != null) {
+					statement.close();
+				}
+				if (result != null) {
+					result.close();
+				}
+				if (statementWareHouse != null) {
+					statementWareHouse.close();
+				}
+				if (resultWareHouse != null) {
+					resultWareHouse.close();
+				}
+
+			} catch (SQLException e) {
+				System.out.println("Khong the tao bang");
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	private int checkTableExist(Connection connection, String table_name, String db_name) {
+		String sql = "SELECT COUNT(*)\r\n" + "FROM information_schema.tables \r\n" + "WHERE table_schema = '" + db_name
+				+ "' \r\n" + "AND table_name = '" + table_name + "';";
+		PreparedStatement statement = null;
+		ResultSet res = null;
+		try {
+			statement = connection.prepareStatement(sql);
+			res = statement.executeQuery();
+			while (res.next()) {
+				return res.getInt(1);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (res != null) {
+					res.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return 0;
+	}
+	public static boolean createTable(MyConfig config, Connection connectionStaging) {
+		boolean isCreated = false;
+		PreparedStatement statement = null;
+		ResultSet res = null;
+		// Tạo ra một mảng từng field trong table
+		String[] fields = config.getVariabless().split(",");
+		
+		// Khỏi tạo câu lệnh sql create table
+		String sql = "Create table " + config.getStaging_table() + " (";
+		
+		// Chạy vòng lặp để hoàn thiện câu lệnh sql
+		for (int i = 0; i < fields.length; i++) {
+			if (i == 0) {
+				sql += fields[i] + " varchar(10) PRIMARY KEY,";
+			} else if (i == fields.length - 1) {
+				sql += fields[i] + " varchar(225));";
+			} else {
+				sql += fields[i] + " varchar(255),";
+			}
+		}
+
+		// Thực hiện câu lệnh sql
+		try {
+			statement = connectionStaging.prepareStatement(sql);
+			statement.execute();
+			System.out.println("Create completed " + config.getStaging_table());
+			isCreated = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (res != null) {
+					res.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+
+			} catch (SQLException e) {
+				System.out.println("Khong the tao bang");
+				e.printStackTrace();
+			}
+		}
+
+		return isCreated;
+
+	}
+
+	public static void main(String[] args) {
+		DataWarehouse dataWarehouse = new DataWarehouse();
+		dataWarehouse.run();
+	}
 
 }
