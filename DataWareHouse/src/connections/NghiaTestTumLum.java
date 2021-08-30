004eed1dcc0a7adf81ab44ab746e39f31655aeaf
tannghia <nghia02584@gmail.com>
2021-08-30 22:19:18 +0700

    linses

--- DataWareHouse/src/connections/NghiaTestTumLum.java
+++ DataWareHouse/src/connections/NghiaTestTumLum.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
  System.out.println("Nghia test 1");
 /**
  
- day la tin nhan toi nghia1
+ day la tin nhan toi nghia1-ban quyen roi thang fake khong dược lấy tin nhan nay
  
  **/
 }
