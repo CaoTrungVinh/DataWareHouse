86a73533230846c6b9eeba86530f7affebd01665
tannghia <nghia02584@gmail.com>
2021-08-30 22:15:57 +0700

    tin nhan toi nghia1

--- DataWareHouse/src/connections/NghiaTestTumLum.java
+++ DataWareHouse/src/connections/NghiaTestTumLum.java
@@ -3,5 +3,10 @@ package connections;
 public class NghiaTestTumLum {
 public static void main(String[] args) {
  System.out.println("Nghia test 1");
+/**
+ 
+ day la tin nhan toi nghia1
+ 
+ **/
 }
 }	
