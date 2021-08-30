9c7d07332bde4a3ee31a27a073823640119f3889
tannghia <nghia02584@gmail.com>
2021-08-30 22:00:11 +0700

    ola ola ola

--- DataWareHouse/src/connections/NghiaTestTumLum.java
+++ DataWareHouse/src/connections/NghiaTestTumLum.java
@@ -3,5 +3,16 @@ package connections;
 public class NghiaTestTumLum {
 public static void main(String[] args) {
  System.out.println("Nghia test 1");
+ 
+ 
+ /**
+  *  day la nhanh dao ok chua
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * **/
 }
 }	
